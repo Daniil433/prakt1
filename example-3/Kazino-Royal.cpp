afdb50a0200752c6f92c309e5b258ed3306a445e
Daniil433 <113330064+Daniil433@users.noreply.github.com>
2022-10-15 15:29:37 +0300

    Update Kazino-Royal.cpp

--- example-3/Kazino-Royal.cpp
+++ example-3/Kazino-Royal.cpp
@@ -1,3 +1,29 @@
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@Daniil433 
+An-Besruky
+/
+Pract_1
+Public
+Code
+Issues
+Pull requests
+Projects
+Security
+Insights
+Pract_1/example-3/Kazino-Royal.cpp
+@MikeLupanov
+MikeLupanov Initial commit
+Latest commit bee4f88 on 12 Apr 2020
+ History
+ 1 contributor
+40 lines (39 sloc)  1.27 KB
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -38,3 +64,18 @@ int main()
     cout<<"Может в любви повезет?\n";
     return 1;
 }
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+prakt1/Kazino-Royal.cpp at master · An-Besruky/prakt1
